5254f14bb8892ff2938b239691d028f30d715c7e
mohamed-safouen <mohamedsafouen@gmail.com>
2022-02-22 01:23:14 +0100

    ramadhan

---
+++ script.js
@@ -0,0 +1,16 @@
+let ramadhan = new Date("april 03, 2022 18:47:52").getTime();
+ func = setInterval(function () {
+    let now = new Date().getTime();
+    let Diff = ramadhan - now;
+
+    let days = Math.floor(Diff / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((Diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((Diff % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((Diff % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days + "d ";
+    document.getElementById("hours").innerHTML = hours + "h ";
+    document.getElementById("mins").innerHTML = minutes + "m ";
+    document.getElementById("secs").innerHTML = seconds + "s ";
+
+})
